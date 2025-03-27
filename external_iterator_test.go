f29f3e47f0d13ba91f2cb2f0c8560893d09bbbe8
Radu Berinde <radu@cockroachlabs.com>
2025-03-27 08:04:26 -0700

    pebble: add missing cleanup in a NewExternalIter error case
    If creating a point or range key iterator fails, we leak some
    iterators that were already opened. This immediately leads to a
    BufferPool-related panic when closing the `Iterator`.
    
    This change adds an enhancement to a test which reproduces the panic
    and patches up the error paths.
    
    Informs github.com/cockroachdb/cockroach/issues/141606

--- external_iterator_test.go
+++ external_iterator_test.go
@@ -5,14 +5,14 @@
 package pebble
 
 import (
-	"bytes"
 	"fmt"
 	"math/rand/v2"
+	"slices"
 	"testing"
 
 	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/pebble/internal/testkeys"
-	"github.com/cockroachdb/pebble/objstorage/objstorageprovider"
 	"github.com/cockroachdb/pebble/sstable"
 	"github.com/cockroachdb/pebble/vfs"
 	"github.com/stretchr/testify/require"
@@ -61,6 +61,7 @@ func TestExternalIterator(t *testing.T) {
 					}
 				}
 			}
+			testExternalIteratorInitError(t, o, &opts, files)
 			it, err := NewExternalIter(o, &opts, files)
 			require.NoError(t, err)
 			return runIterCmd(td, it, true /* close iter */)
@@ -70,72 +71,41 @@ func TestExternalIterator(t *testing.T) {
 	})
 }
 
-func BenchmarkExternalIter_NonOverlapping_Scan(b *testing.B) {
-	ks := testkeys.Alpha(6)
-	opts := &Options{Comparer: testkeys.Comparer}
-	opts.EnsureDefaults()
-	iterOpts := &IterOptions{
-		KeyTypes: IterKeyTypePointsAndRanges,
+// testExternalIteratorInitError tests error handling paths inside
+// NewExternalIter by injecting errors when reading files.
+//
+// See github.com/cockroachdb/cockroach/issues/141606 where an error during
+// initialization caused NewExternalIter to panic.
+func testExternalIteratorInitError(
+	t *testing.T, o *Options, iterOpts *IterOptions, files [][]sstable.ReadableFile,
+) {
+	files = slices.Clone(files)
+	for i := range files {
+		files[i] = slices.Clone(files[i])
+		for j := range files[i] {
+			files[i][j] = &flakyFile{ReadableFile: files[i][j]}
+		}
 	}
-	writeOpts := opts.MakeWriterOptions(6, sstable.TableFormatPebblev2)
-	var valBuf [512]byte
 
-	for _, keyCount := range []int{100, 10_000, 100_000} {
-		b.Run(fmt.Sprintf("keys=%d", keyCount), func(b *testing.B) {
-			for _, fileCount := range []int{1, 10, 100} {
-				b.Run(fmt.Sprintf("files=%d", fileCount), func(b *testing.B) {
-					prng := rand.New(rand.NewPCG(0, 0))
-
-					var fs vfs.FS = vfs.NewMem()
-					filenames := make([]string, fileCount)
-					var keys [][]byte
-
-					for i := 0; i < fileCount; i++ {
-						filename := fmt.Sprintf("%03d.sst", i)
-						wf, err := fs.Create(filename, vfs.WriteCategoryUnspecified)
-						require.NoError(b, err)
-						w := sstable.NewWriter(objstorageprovider.NewFileWritable(wf), writeOpts)
-						for j := 0; j < keyCount/fileCount; j++ {
-							key := testkeys.Key(ks, int64(len(keys)))
-							keys = append(keys, key)
-							for i := range valBuf {
-								valBuf[i] = byte(prng.Uint32())
-							}
-							require.NoError(b, err)
-							require.NoError(b, w.Set(key, valBuf[:]))
-						}
-						require.NoError(b, w.Close())
-						filenames[i] = filename
-					}
-
-					b.ResetTimer()
-					for i := 0; i < b.N; i++ {
-						func() {
-							files := make([][]sstable.ReadableFile, fileCount)
-							for i := 0; i < fileCount; i++ {
-								f, err := fs.Open(filenames[i])
-								require.NoError(b, err)
-								files[i] = []sstable.ReadableFile{f}
-							}
-
-							it, err := NewExternalIter(opts, iterOpts, files)
-							require.NoError(b, err)
-							defer it.Close()
-
-							k := 0
-							for valid := it.First(); valid; valid = it.NextPrefix() {
-								if !bytes.Equal(it.Key(), keys[k]) {
-									b.Fatalf("expected key %q, found %q", keys[k+1], it.Key())
-								}
-								k++
-							}
-							if k != len(keys) {
-								b.Fatalf("k=%d, expected %d\n", k, len(keys))
-							}
-						}()
-					}
-				})
-			}
-		})
+	for iter := 0; iter < 100; iter++ {
+		it, err := NewExternalIter(o, iterOpts, files)
+		if err != nil {
+			require.Contains(t, err.Error(), "flaky file")
+		} else {
+			it.Close()
+		}
 	}
 }
+
+type flakyFile struct {
+	sstable.ReadableFile
+}
+
+func (ff *flakyFile) ReadAt(p []byte, off int64) (n int, err error) {
+	if rand.IntN(10) == 0 {
+		return 0, errors.New("flaky file")
+	}
+	return ff.ReadableFile.ReadAt(p, off)
+}
+
+func (ff *flakyFile) Close() error { return nil }
